3e37a1e211b86a2881d91ab439a1f98ffa324f02
NiWayanCiliWangi <220030328@stikom-bali.ac.id>
2024-01-23 00:52:13 +0800

    first commit

--- Admin/footer.php
+++ Admin/footer.php
@@ -1,4 +1,4 @@
 <footer>
     <a href="../logout.php">Logout</a>
-    <p>Hubungi kami di: infohealingyoursoul.gmail.com | Phone: (123) 456-7890</p>
+    <p>Hubungi kami di: infohealingyoursoul.gmail.com | Phone: 081949820618</p>
 </footer>
